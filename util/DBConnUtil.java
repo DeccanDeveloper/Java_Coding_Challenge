b9d4269f381f70e36fb55d6513f8b49a691b2496
Manovishaal D <manovishaaldhayalanathan@gmail.com>
2025-04-18 11:11:58 +0530

    Delete util directory

--- util/DBConnUtil.java
+++
@@ -1,4 +0,0 @@
-public static Connection getDBConn() throws Exception {
-    String connStr = DBPropertyUtil.getConnectionString("db.properties");
-    return DriverManager.getConnection(connStr);
-}
